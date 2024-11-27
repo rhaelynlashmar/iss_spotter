dc2149d60e28757433edd2cc97c80fd218d5d5ff
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-27 11:46:33 -0800

    Added 'body' as a callback parameter, and notes over 2nd error-handlng

--- iss.js
+++ iss.js
@@ -7,17 +7,18 @@ const needle = require('needle');
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
-const fetchMyIP = function(callback) { 
+const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
-  const url = 'https://api.ipify.org?format=json'; 
+  const url = 'https://api.ipify.org?format=json';
   
   // Make HTTP request using needle
-  needle.get(url, (error, response) => {
+  needle.get(url, (error, response, body) => {
     if (error) {
       callback(error, null); // if error occurs, pass it to the callback
       return;
     }
 
+    // If non-200 status, assume server error
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
@@ -25,7 +26,7 @@ const fetchMyIP = function(callback) {
     }
 
     // Needle parses JSON for us, so we can access response.body
-    callback(null, response.body.ip) // pass IP to callback
+    callback(null, response.body.ip); // pass IP to callback
   });
 };
 
