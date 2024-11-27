e1ac1d287c555aef1a212d444303ccd660ee8f2c
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-27 13:54:50 -0800

    Added testing for fetchMyCoordsByIP with dummy IP address

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const { fetchMyIP } = require('./iss');
+const { fetchMyCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+const IP = "216.71.199.190";
+
+fetchMyCoordsByIP(IP, (error, data) => {
   if (error) {
-    console.log("It didn't work!" , error);
+    console.log("It didn't work!", error);
     return;
   }
-
-  console.log('It worked! Returned IP:' , ip);
+  console.log("Valid IP - Coordinates", data);
 });
 
